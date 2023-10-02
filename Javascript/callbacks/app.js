a30252bfb96b79f0d4cec17f6e144528dc8742ac
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-10-02 17:28:41 -0400

    async and callbacks
    this and that

--- Javascript/callbacks/app.js
+++ Javascript/callbacks/app.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+
+function asyncHandler(cb) {
+    return async (req, res, next) => {
+        try {
+            await cb(req, res, next);
+        }catch(err){
+            res.render("error", {error:err});
+        }
+    }
+
+}
+
 
 //CALL BACKS
 // function getUsers(cb){
@@ -24,21 +37,38 @@ app.use(express.static('public'));
 //     }else{
 //       res.render("index", {title: "Users", users: users.users});
 //     }
-//   });    
+//   });
 // });
 
-
 // PROMISES
-function getUsers(){
-  
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("data.json", "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const users = JSON.parse(data);
+        resolve(users);
+      }
+    });
+  });
 }
 
-app.get('/', (req,res) => {    
-  
-});
+// app.get("/", (req, res) => {
+//     getUsers()
+//     .then((users) => {
+//         res.render("index", { title: "Users", users: users.users });
+//     })
+//     .catch((err)=> {
+//         res.render("error", {error:err});
+//     })
+// });
+
+// async/await
+app.get ("/", asyncHandler(async (req, res)=> {    
+    const users = await getUsers();
+    res.render("index", { title: "Users", users: users.users });    
+}))
 
 
-
-
-
-app.listen(3000, () => console.log('App listening on port 3000!'));
+app.listen(3000, () => console.log("App listening on port 3000!"));
